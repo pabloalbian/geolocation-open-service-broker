58b9ca4020184302a10296803af1f872f41bb7c1
pabloalbian <pabloalbendea3@gmail.com>
2021-05-22 18:31:53 +0200

    First commit

---
+++ src/main/java/com/example/geolocationopenservicebroker/GeolocationOpenServiceBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.example.geolocationopenservicebroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocationOpenServiceBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocationOpenServiceBrokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geolocationopenservicebroker/GeolocationOpenServiceBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geolocationopenservicebroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocationOpenServiceBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
